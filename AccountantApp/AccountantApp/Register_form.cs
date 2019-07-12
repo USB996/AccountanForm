74ff84d9764613a0ebb1969340c01f2c02b4565a
ucha.sibashvili@hotmail.com <ucha.sibashvili@hotmail.com>
2019-07-12 17:15:51 +0400

    Added Json

--- AccountantApp/AccountantApp/Register_form.cs
+++ AccountantApp/AccountantApp/Register_form.cs
@@ -7,29 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace AccountantApp
 {
     public partial class Register_form : Form
     {
-        
+            List<User> users = new List<User>();
+            
+
 
         public Register_form()
         {
             InitializeComponent();
-            List<User> users = new List<User>();
-            users.Add(new User()
-            {
-                FirstName = Name_bx.Text,
-                LastName=Lst_nm_bx.Text,
-                ID=ID_Bx.Text,
-                UserName=usrNm_bx.Text,
-                Password=Pswrd_bx.Text,
-                RepeatPassword=Rpt_pswrd_bx.Text
-
-
-            });
-
+            
         }
 
 
@@ -48,6 +40,31 @@ namespace AccountantApp
         private void Save_Btn_Click(object sender, EventArgs e)
         {
             PasswordValidation();
+            users.Add(new User()
+            {
+                FirstName = Name_bx.Text,
+                LastName = Lst_nm_bx.Text,
+                ID = ID_Bx.Text,
+                UserName = usrNm_bx.Text,
+                Password = Pswrd_bx.Text,
+                RepeatPassword = Rpt_pswrd_bx.Text
+
+            });
+
+            string json = JsonConvert.SerializeObject(users);
+
+            Name_bx.Text = "";
+            Lst_nm_bx.Text = "";
+            ID_Bx.Text = "";
+            Pswrd_bx.Text = "";
+            Rpt_pswrd_bx.Text = "";
+            usrNm_bx.Text = "";
+            File.WriteAllText(@"UsersData.txt", json);
+            MessageBox.Show("Succesfully Registered");
+
+
         }
+
+        
     }
 }
